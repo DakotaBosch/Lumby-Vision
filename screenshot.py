5395944ac4d9991ac8d6ce17956c9959d36be6e7
DakotaBosch <59891614+DakotaBosch@users.noreply.github.com>
2022-07-17 23:06:59 -0400

    added dataset

--- screenshot.py
+++ screenshot.py
@@ -8,7 +8,7 @@ from PIL import Image
 start_time = time.time()
 
 #screenshot location
-mon = {'top': 200, 'left': 300, 'width': 1200, 'height': 800}
+mon = {'top': 40, 'left': 60, 'width': 1800, 'height': 1000}
 
 duration = np.empty((1,))
 
@@ -26,7 +26,7 @@ with mss.mss() as sct:
         #img = Image.frombytes('RGB', im.size, im.rgb)
 
         #compress PIL image to (X,Y)
-        img = img.resize((600,400))
+        img = img.resize((900,500))
         
         
         #sct.compression_level = 9
@@ -37,14 +37,15 @@ with mss.mss() as sct:
         #print('The loop took: {:.0f}'.format(time.time()*1000-last_time*1000) + ' ms')
 
         cv2.imshow('test', np.array(img))
-        
+
+
         #break cycle w/ user input 'q'
         if cv2.waitKey(25) & 0xFF == ord('q'):
-            #why are blue and red opposite of the live output
+                #why are blue and red opposite of the live output
             img.save('screenshot.png')
             cv2.destroyAllWindows()
             break
     
-    print(str(wid) + "x" + str(hgt))
 
+    print(str(wid) + "x" + str(hgt))
     print('Average time for each image: {:.0f}'.format(np.mean(duration)) + 'ms')
